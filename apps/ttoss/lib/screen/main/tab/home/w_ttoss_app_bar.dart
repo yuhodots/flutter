b336787db3196bd0b01b223fc4b1426496d72f29
Yuho Jeong <yuho8437@gmail.com>
2024-10-06 23:56:09 +0900

    feat: add one more app

---
+++ apps/ttoss/lib/screen/main/tab/home/w_ttoss_app_bar.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import '../../../../common/common.dart';
+import '../../../notification/s_notification.dart';
+
+class TtossAppBar extends StatefulWidget {
+  static const double appBarHeight = 60;
+
+  const TtossAppBar({super.key});
+
+  @override
+  State<TtossAppBar> createState() => _TtossAppBarState();
+}
+
+class _TtossAppBarState extends State<TtossAppBar> {
+  bool _showRedDot = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: TtossAppBar.appBarHeight,
+      color: context.appColors.appBarBackground,
+      child: Row(
+        children: [
+          width10,
+          Image.asset(
+            "$basePath/icon/toss.png",
+            height: 30,
+          ),
+          emptyExpanded,
+          Image.asset(
+            "$basePath/icon/map_point.png",
+            height: 30,
+          ),
+          width10,
+          Tap(
+            onTap: () {
+              //알림 화면
+              Nav.push(const NotificationScreen());
+            },
+            child: Stack(
+              children: [
+                Image.asset(
+                  "$basePath/icon/notification.png",
+                  height: 30,
+                ),
+                if (_showRedDot)
+                  Positioned.fill(
+                      child: Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      width: 6,
+                      height: 6,
+                      decoration: const BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+                    ),
+                  ))
+              ],
+            ),
+          ),
+          width10,
+        ],
+      ),
+    );
+  }
+}
